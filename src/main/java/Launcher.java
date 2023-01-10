86c64975ea7f46f566ae48cb99682f72583e4548
jack <jaksonmillka@gmail.com>
2023-01-10 19:45:34 +0100

    Add TheMainGallows class

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,13 @@
+/**
+ * @author Evgeniy_Tyslevich
+ * @link https://github.com/dublXq
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+
+
+        TheMainGallows theMainGallows = new TheMainGallows();
+        theMainGallows.mainMaps();
+    }
+}
---
+++ src/main/java/TheMainGallows.java
@@ -0,0 +1,15 @@
+/**
+ * @author Evgeniy_Tyslevich
+ * @link https://github.com/dublXq
+ */
+public class TheMainGallows {
+
+    public void mainMaps () {
+        System.out.println("                _____" +
+                "\n               |     |"+
+                "\n                     |"+
+                "\n                     |"+
+                "\n                 _______");
+    }
+
+}
